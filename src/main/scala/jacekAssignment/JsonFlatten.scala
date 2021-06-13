0c44d71fb2cb105b3cf03dca29c6f4b1b07e4c2e
sunny30 <sharadsingh8@gmail.com>
2021-06-13 16:49:33 +0530

    lineage and default argument for flatten

--- src/main/scala/jacekAssignment/JsonFlatten.scala
+++ src/main/scala/jacekAssignment/JsonFlatten.scala
@@ -18,9 +18,9 @@ object JsonFlatten {
     df.select(col("bike.name").as("bike_name"), col("age"),col("name"), explode(col("bike.models")).as("bike_model")).show()
     val flatDF = flattenDF(df)
 
-    val fdf = df.flattenDF(df)
+    val fdf = df.flattenDF()
 
-    flatDF.show()
+    fdf.show()
   }
 
 
@@ -55,10 +55,10 @@ object JsonFlatten {
 
   implicit class DF(df:DataFrame){
 
-    implicit def flattenDF(df:DataFrame):DataFrame={
-      val fields = df.schema.fields
-      val len = df.schema.length
-      val fieldNames = df.schema.fields.map(x=>x.name)
+    implicit def flattenDF(df1:DataFrame=df):DataFrame={
+      val fields = df1.schema.fields
+      val len = df1.schema.length
+      val fieldNames = df1.schema.fields.map(x=>x.name)
 
       for(i<-0 to len-1){
         val cfield = fields(i)
@@ -66,13 +66,13 @@ object JsonFlatten {
 
           case arrayType: ArrayType =>{
             val columns = fieldNames.filter(_!=cfield.name).map(x=>expr(x))++Seq(explode(col(cfield.name)).as(cfield.name))
-            val flatdf = df.select(columns:_*)
+            val flatdf = df1.select(columns:_*)
             return flattenDF(flatdf)
 
           }
           case structType: StructType=>{
             val columns = fieldNames.filter(_!=cfield.name).map(x=>expr(x))++structType.fields.map(x=> col(cfield.name+"."+x.name).as(cfield.name+"_"+x.name))
-            val flatDF = df.select(columns:_*)
+            val flatDF = df1.select(columns:_*)
             return flattenDF(flatDF)
 
           }
@@ -81,7 +81,7 @@ object JsonFlatten {
         }
 
       }
-      df
+      df1
     }
 
 
